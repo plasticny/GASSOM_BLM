cfeade17911b7af8a66059fe5a5e50d26dd9a44c
plasticny <79622161+plasticny@users.noreply.github.com>
2025-09-17 17:12:16 +0800

    normalize projection when selecting winner

--- Class/GASSOM_Online.m
+++ Class/GASSOM_Online.m
@@ -61,32 +61,37 @@ classdef GASSOM_Online  < handle
             obj.sigma_n = 0.2;
             obj.sigma_w = 2;
             
-            obj.alpha_A = 1; % magnitude
-            obj.alpha_C = 1;
-            % obj.alpha_A = 8e-4; % magnitude
-            % obj.alpha_C = 1e-5;
+            % obj.alpha_A = 1; % magnitude
+            % obj.alpha_C = 1e-3;
+            obj.alpha_A = 8e-4; % magnitude
+            obj.alpha_C = 1e-5;
+
             % obj.sigma_A = 2;
             % obj.sigma_C = .1;
             obj.sigma_A = 2;
             obj.sigma_C = .2;
-            % obj.tconst = 8000;            
-            obj.tconst = 50000;
+
+            % obj.tconst = 8000;         
+            obj.tconst = 10000;
             
             obj.transProb =  genTransProbG(obj.topo_subspace,obj.sigmaTrans, obj.alphaTrans,0); 
+            np = rand(obj.n_subspace,1);    
+            obj.nodeProb = bsxfun(@rdivide,np,sum(np));
+
             %random initial bases                       
-            save_name = sprintf('temp_data/InitBases_%d_%d_B.mat',obj.length_basis,obj.n_subspace);
-            if ~exist(save_name,'file')
-                A = randn(obj.length_basis, obj.size_subspace, obj.n_subspace);
-                A = orthonormalize_subspace (A);
-                obj.bases{1}= squeeze(A(:,1,:)); obj.bases{2}= squeeze(A(:,2,:));                
-                np = rand(obj.n_subspace,1);    
-                obj.nodeProb = bsxfun(@rdivide,np,sum(np));
-                save(save_name,'A','np');
-            else
-                load(save_name,'A','np');
-                obj.bases{1}= squeeze(A(:,1,:)); obj.bases{2}= squeeze(A(:,2,:));
-                obj.nodeProb = bsxfun(@rdivide,np,sum(np));    
-            end           
+            % save_name = sprintf('temp_data/InitBases_%d_%d_B.mat',obj.length_basis,obj.n_subspace);
+            % if ~exist(save_name,'file')
+            A = randn(obj.length_basis, obj.size_subspace, obj.n_subspace);
+            A = orthonormalize_subspace (A);
+            obj.bases{1}= squeeze(A(:,1,:));
+            obj.bases{2}= squeeze(A(:,2,:));
+
+            %     save(save_name,'A','np');
+            % else
+            %     load(save_name,'A','np');
+            %     obj.bases{1}= squeeze(A(:,1,:)); obj.bases{2}= squeeze(A(:,2,:));
+            %     obj.nodeProb = bsxfun(@rdivide,np,sum(np));    
+            % end           
             obj.iter = 1;
             obj.updatecount = 0;
             %init visualization
@@ -103,7 +108,19 @@ classdef GASSOM_Online  < handle
             this.coef{1} = this.bases{1}'*X; %[n_subspace batch_size]
             this.coef{2} = this.bases{2}'*X;
 
-            this.Proj  = this.coef{1}.^2 + this.coef{2}.^2; %P[n_subspace,batch_size]
+            % disp(max(this.coef{1}));
+            % disp(min(this.coef{1}));
+
+            % disp(max(this.coef{2}));
+            % disp(min(this.coef{2}));
+
+            this.Proj = this.coef{1}.^2 + this.coef{2}.^2; %P[n_subspace,batch_size]
+            this.Proj = this.Proj./max(this.Proj);
+
+            % disp(max(this.Proj, [], "all"));
+            assert(max(this.Proj, [], "all") <= 1);
+            % assert(false);
+            
             Perr = ones(size(this.Proj))-this.Proj;
             emissProb=exp(-this.Proj/(2*this.sigma_w^2)).*exp(-Perr/(2*this.sigma_n^2));      
 
@@ -113,6 +130,7 @@ classdef GASSOM_Online  < handle
                 this.nodeProb =  nodeprobTmp(:,i)./sum(nodeprobTmp(:,i));
             end       
             [~,this.winners] = max(nodeprobTmp);
+
             winners = this.winners;
         end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -129,7 +147,7 @@ classdef GASSOM_Online  < handle
                 resp = coef1.^2 + coef2.^2;
             end
         end
-        
+
         function [basis] = getBasis(this,ind,i_subspace)
             if nargin<3, i_subspace = 1; end
             basis_ind = this.bases{i_subspace}(:,ind);
@@ -145,53 +163,11 @@ classdef GASSOM_Online  < handle
 %            updateBasis
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%           
         
-        % function updateBasis(this,X)          
-            
-        %     alpha = (this.alpha_A*exp(-this.iter/this.tconst)+this.alpha_C);
-        %     sigma_h = (this.sigma_A*exp(-this.iter/this.tconst)+this.sigma_C);           
-           
-        %     batch_size = size(X,2);
-        %     [cj,ci] = ind2sub(this.topo_subspace,this.winners);   
-        %     k = 1:this.n_subspace;
-        %     [kj,ki] = ind2sub(this.topo_subspace,k);
-
-        %     kj = repmat(kj',[1,batch_size] );
-        %     ki = repmat(ki',[1,batch_size] );
-
-        %     cj = repmat(cj, [this.n_subspace,1]);
-        %     ci = repmat(ci, [this.n_subspace,1]);
-
-        %     func_h = exp((-(ki-ci).^2-(kj-cj).^2)/(2*(sigma_h)^2)); %gaussian [n_subspace,batchsize]
-            
-        %     n_const = 1./(sqrt(this.Proj)+eps);
-        %     weights = func_h.*n_const;
-        %     w_c{1} =weights.*this.coef{1};
-        %     w_c{2} =weights.*this.coef{2};
-            
-        %     winput{1} = X*w_c{1}';
-        %     winput{2} = X*w_c{2}';
-            
-        %     diff{1} =  winput{1}-bsxfun(@times,this.bases{1},sum(w_c{1}.*this.coef{1},2)')-bsxfun(@times,this.bases{2},sum(w_c{1}.*this.coef{2},2)');
-        %     diff{2} =  winput{2}-bsxfun(@times,this.bases{1},sum(w_c{2}.*this.coef{1},2)')-bsxfun(@times,this.bases{2},sum(w_c{2}.*this.coef{2},2)');
-           
-        %     Bases{1} = this.bases{1} +alpha*diff{1};
-        %     Bases{2} = this.bases{2} +alpha*diff{2};
-
-        %     this.bases{1} = bsxfun(@rdivide, Bases{1}, sqrt(sum(Bases{1}.^2)));
-        %     Bases{2} = Bases{2} - bsxfun(@times,this.bases{1}, sum(this.bases{1}.*Bases{2}));
-        %     this.bases{2} = bsxfun(@rdivide, Bases{2}, sqrt(sum(Bases{2}.^2)));            
-            
-        %     this.iter = this.iter+1; 
-        %     this.updatecount = this.updatecount+1;
-        % end
-
         function updateBasis(this,X)          
-            
             alpha = (this.alpha_A*exp(-this.iter/this.tconst)+this.alpha_C);
-            sigma_h = (this.sigma_A*exp(-this.iter/this.tconst)+this.sigma_C);
+            sigma_h = (this.sigma_A*exp(-this.iter/this.tconst)+this.sigma_C);           
            
-           
-            [~, batch_size] = size(X);
+            batch_size = size(X,2);
             [cj,ci] = ind2sub(this.topo_subspace,this.winners);   
             k = 1:this.n_subspace;
             [kj,ki] = ind2sub(this.topo_subspace,k);
@@ -207,27 +183,21 @@ classdef GASSOM_Online  < handle
             n_const = 1./(sqrt(this.Proj)+eps);
             weights = func_h.*n_const;
             w_c{1} =weights.*this.coef{1};
-            %w_c{2} =weights.*this.coef{2};
+            w_c{2} =weights.*this.coef{2};
             
             winput{1} = X*w_c{1}';
-            %winput{2} = X*w_c{2}';
+            winput{2} = X*w_c{2}';
             
-            diff{1} =  winput{1}-bsxfun(@times,this.bases{1},sum(w_c{1}.*this.coef{1},2)'); 
-            %+ this.coef{2}.^2; %P[n_subspace,batch_size]
-           
-            %diff{2} =  winput{2}-bsxfun(@times,this.bases{1},sum(w_c{2}.*this.coef{1},2)')-bsxfun(@times,this.bases{2},sum(w_c{2}.*this.coef{2},2)');
+            diff{1} =  winput{1}-bsxfun(@times,this.bases{1},sum(w_c{1}.*this.coef{1},2)')-bsxfun(@times,this.bases{2},sum(w_c{1}.*this.coef{2},2)');
+            diff{2} =  winput{2}-bsxfun(@times,this.bases{1},sum(w_c{2}.*this.coef{1},2)')-bsxfun(@times,this.bases{2},sum(w_c{2}.*this.coef{2},2)');
            
             Bases{1} = this.bases{1} +alpha*diff{1};
-            %Bases{2} = this.bases{2} +alpha*diff{2};	
+            Bases{2} = this.bases{2} +alpha*diff{2};
 
             this.bases{1} = bsxfun(@rdivide, Bases{1}, sqrt(sum(Bases{1}.^2)));
-            %Bases{2} = Bases{2} - bsxfun(@times,this.bases{1}, sum(this.bases{1}.*Bases{2}));
-            %this.bases{2} = bsxfun(@rdivide, Bases{2}, sqrt(sum(Bases{2}.^2)));            
-           
-            % normalize bases
-            this.bases{1}=this.bases{1}./vecnorm(this.bases{1});
-            %this.bases{2}=this.bases{2}./vecnorm(this.bases{2});
-
+            Bases{2} = Bases{2} - bsxfun(@times,this.bases{1}, sum(this.bases{1}.*Bases{2}));
+            this.bases{2} = bsxfun(@rdivide, Bases{2}, sqrt(sum(Bases{2}.^2)));            
+            
             this.iter = this.iter+1; 
             this.updatecount = this.updatecount+1;
         end
